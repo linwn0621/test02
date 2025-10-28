667c425f6144ad86ea42315aa287a5ad983301e3
18162892 <18162892@karrie.com>
2025-10-28 14:49:15 +0800

    新增

---
+++ test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="">
+        111
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    data() {
+        return {}
+    },
+    components: {
+
+    },
+    computed: {
+
+    },
+    mounted: {
+
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style scoped lang='less'></style>
